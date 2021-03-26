3a7261758c52279f4cecbbe9a8a7b9d92276ecbd
Mason <masonbe25@gmail.com>
2021-03-26 18:16:08 -0400

    Added Base Files

---
+++ src/EZTranslate.java
@@ -0,0 +1,17 @@
+/**
+ * Starts the applications functions. Initializes application manager
+ * @author mason
+ *
+ */
+public class EZTranslate {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Starting Application");//starting message
+		
+		
+		
+		System.out.println("Application Ended");//ending message
+	}//main
+
+}//class
---
+++ src/TranslationService.java
@@ -0,0 +1,8 @@
+/**
+ * Contains interactions with language detection and translation library.
+ * @author mason
+ *
+ */
+public class TranslationService {
+
+}
